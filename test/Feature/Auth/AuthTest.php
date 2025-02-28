8650801d1ecda03899500e214c62bbbb7b96b0c4
Luiz Carvalho <luizottavio49@gmail.com>
2025-02-27 21:06:39 -0300

    clean: removing client from AuthTest

--- test/Feature/Auth/AuthTest.php
+++ test/Feature/Auth/AuthTest.php
@@ -4,21 +4,10 @@ declare(strict_types=1);
 
 namespace Test\Feature\Auth;
 
-use Hyperf\Testing\Client;
 use Hyperf\Testing\TestCase;
 
-use function Hyperf\Support\make;
-
 class AuthTest extends TestCase
-{
-    protected Client $client;
-
-    public function __construct($name = null, array $data = [], $dataName ='')
-    {
-        parent::__construct($name, $data, $dataName);
-        $this->client = make(Client::class);
-    }
-    
+{   
     public function testLoginAdmin(): void
     {
         $response = $this->post('/auth/login', [
